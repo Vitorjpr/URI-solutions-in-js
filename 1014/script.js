4edfd1cbc980a67674a28ca20667968cb58652f3
Vitorjpr <vitorjprodrigues97@gmail.com>
2020-07-02 18:47:59 -0300

    [URI-SOLUTIONS] - Subindo solução para exercicio 1014.

--- 1014/script.js
+++ 1014/script.js
@@ -1,8 +1,8 @@
 var input = require('fs').readFileSync('1014/stdin', 'utf8');
 var lines = input.split('\n');
 
-var distanciaPercorrida = lines.shift();
-var combustivelGasto = lines.shift();
-var consumoMedio = distanciaPercorrida / combustivelGasto;
+var distanciaPercorridaEmKm = lines.shift();
+var combustivelGastoEmLitros = lines.shift();
+var consumoMedio = distanciaPercorridaEmKm / combustivelGastoEmLitros;
 
 console.log(consumoMedio.toFixed(3) + ' km/l');
