d2f2eab6e2c67aa4061af16c286d7883fa1899be
Vitor Rodrigues <p000vitor.jose.rodrigues@prservicos.com.br>
2020-07-02 18:37:41 -0300

    [URI-SOLUTIONS] - Subindo solução para exercicio 1013.

---
+++ 1013/script.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('1013/stdin', 'utf8');
+var lines = input.split('\n');
+
+var [ a, b, c ] = lines.shift().split(' ');
+
+var maiorAb = (parseInt(a) + parseInt(b) + Math.abs(a - b)) / 2;
+var maiorAbc = (parseInt(maiorAb) + parseInt(c) + Math.abs(maiorAb - c)) / 2;
+
+console.log(maiorAbc + " eh o maior");
